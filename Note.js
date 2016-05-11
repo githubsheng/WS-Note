41531184ce4e3d2c6811ad45d958dbce41eb6c43
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-11 14:23:47 +0800

    add model class note

--- Note.js
+++ Note.js
@@ -2,5 +2,9 @@
  * Created by wangsheng on 7/5/16.
  */
 class Note {
+    constructor(createdWhen, modifiedWhen) {
+        this.createdWhen = createdWhen;
+        this.modifiedWhen = modifiedWhen;
+    }
 }
 //# sourceMappingURL=Note.js.map
--- Note.ts
+++ Note.ts
@@ -3,5 +3,16 @@
  */
 
 class Note {
-    
+
+    public id:number;
+    public title: string;
+    public content: string;
+    public createdWhen: number;
+    public modifiedWhen: number;
+
+    constructor(createdWhen: number, modifiedWhen: number) {
+        this.createdWhen = createdWhen;
+        this.modifiedWhen = modifiedWhen;
+    }
+
 }
