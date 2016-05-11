0a94d7814ffdd911f0c305b564813e8bb800146a
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-11 14:19:41 +0800

    function to run generator

---
+++ AsyncUtil.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by wangsheng on 11/5/16.
+ */
+
+function runGenerator(genFunc:() => IterableIterator<any>) {
+
+    let iterator = genFunc();
+    let result:IteratorResult<any>;
+
+    function iterate(value?:any) {
+        result = iterator.next(value);
+        if (!result.done) {
+            if (result.value instanceof Promise) {
+                result.value.then(iterate);
+            } else {
+                iterate(result.value);
+            }
+        }
+    }
+
+    iterate();
+}
