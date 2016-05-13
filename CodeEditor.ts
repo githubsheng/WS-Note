55c76c9707304f07df64f3512a2e7f4fc18c0bf6
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-13 10:58:38 +0800

    add a dummy code editor implementation

--- CodeEditor.ts
+++ CodeEditor.ts
@@ -1,3 +1,19 @@
-/**
- * Created by wangsheng on 7/5/16.
- */
+interface CodeEditor {
+    containerEle: HTMLElement;
+    getValue: () => string;
+}
+
+function createCodeEditor(): CodeEditor {
+
+    let editor = document.createElement("textarea");
+
+    function getValue(){
+        return editor.value;
+    }
+
+    return {
+        containerEle: editor,
+        getValue: getValue
+    };
+
+}
