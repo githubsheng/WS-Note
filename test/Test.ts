a50803eb8a9c8fbe2f345ece88c638280fa83cce
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-17 15:31:04 +0800

    code reformat

--- test/Test.ts
+++ test/Test.ts
@@ -1,9 +1,7 @@
-/**
- * Created by wangsheng on 16/5/16.
- */
-
-/// <reference path="TestTextProcessor.ts" />
-/// <reference path="TestIndex.ts" />
+///<reference path="TestStorage.ts"/>
+///<reference path="TestTextProcessor.ts" />
+///<reference path="TestIndex.ts" />
 
 runIndexTest();
 runTextProcessorTest();
+runStorageTest();
