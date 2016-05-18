0718bc8d80fe5b16e6ad144a0b2db880e92f2c9b
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-18 21:11:12 +0800

    experimental ui component test

--- test/Test.ts
+++ test/Test.ts
@@ -1,7 +1,11 @@
 ///<reference path="TestStorage.ts"/>
 ///<reference path="TestTextProcessor.ts" />
 ///<reference path="TestIndex.ts" />
+///<reference path="../CodeEditor.ts"/>
 
 runIndexTest();
 runTextProcessorTest();
 runStorageTest();
+
+var editor = createCodeEditor();
+document.body.appendChild(editor.containerEle);
