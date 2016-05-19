2dd96d6fb79a91456841b8231cfcb87a01b88c6e
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-19 16:23:40 +0800

    FreeDraw contains a canvas that is built on top an image

--- FreeDraw.ts
+++ FreeDraw.ts
@@ -1,3 +1,20 @@
 /**
  * Created by wangsheng on 7/5/16.
  */
+
+class FreeDraw {
+    private imgId: number;
+    public canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+
+    constructor(imgId: number, img: HTMLImageElement){
+        this.imgId = imgId;
+        let canvas = document.createElement("canvas");
+        this.canvas = canvas;
+        canvas.width = 100;
+        canvas.height = 100;
+        let ctx = canvas.getContext("2d");
+        this.ctx = ctx;
+        ctx.drawImage(img, 0, 0);
+    }
+}
