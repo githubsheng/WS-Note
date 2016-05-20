ed2b42861ccbe455951176af54e84ce1c2669b06
Wang Sheng <wang.sheng.unique@gmail.com>
2016-05-21 00:24:34 +0800

    test cases for code editor get/set value

--- test/TestCodeEditor.ts
+++ test/TestCodeEditor.ts
@@ -1,15 +1,19 @@
 ///<reference path="../Storage.ts"/>
 ///<reference path="../CodeEditor.ts"/>
 
+var globalCodeEditor;
+
 namespace TestCodeEditorNamespace {
     import getIDB = StorageNamespace.getIDB;
     import storeImageBlob = StorageNamespace.storeImageBlob;
     import createCodeEditor = CodeEditorNamespace.createCodeEditor;
+    import Component = ContentTransformerNamespace.Component;
 
     export function runCodeEditorTest(){
         r(function*(){
             let idb = yield getIDB();
             let codeEditor = createCodeEditor(idb, storeImageBlob);
+            globalCodeEditor = codeEditor;
             document.body.appendChild(codeEditor.containerEle);
 
             var toggleImageInsertButton = document.createElement("button");
@@ -17,7 +21,24 @@ namespace TestCodeEditorNamespace {
             toggleImageInsertButton.onclick = function(){
                 codeEditor.startInsertingImg();
             };
+
+            var root: Component;
+            var getValueAndClearButton = document.createElement("button");
+            getValueAndClearButton.innerText = "Get value and clear";
+            getValueAndClearButton.onclick = function(){
+                root = codeEditor.getValue();
+                codeEditor.clearContent();
+            };
+
+            var setValueButton = document.createElement("button");
+            setValueButton.innerText = "Set value";
+            setValueButton.onclick = function(){
+                if(root) codeEditor.setValue(root);
+            };
+
             document.body.appendChild(toggleImageInsertButton);
+            document.body.appendChild(getValueAndClearButton);
+            document.body.appendChild(setValueButton);
         });
     }
 }
