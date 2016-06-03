5bcffdfbab9467f04c7b8bc614bb2f6eb2497dc8
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-03 23:53:19 +0800

    add AppEvents, different sections of app should communicate through app events

---
+++ AppEvents.ts
@@ -0,0 +1,20 @@
+namespace AppEventsNamespace {
+
+    export enum AppEvent {createNewNote, setBlankResultsPage}
+
+    let eventListenerMap: Map<AppEvent, Array<(data? : any) => void>> = new Map();
+
+    export function register(event: AppEvent, func: (data? : any) => void) {
+        if(eventListenerMap.get(event) === undefined) eventListenerMap.set(event, []);
+        eventListenerMap.get(event).push(func);
+    }
+
+    export function broadcast(event: AppEvent, data?: any) {
+        let listeners = eventListenerMap.get(event);
+        if(listeners !== undefined) {
+            for(let listener of listeners)
+                listener(data);
+        }
+    }
+
+}
