44b931d89c5ca393144e20832007722887bba9a7
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-05 23:58:55 +0800

    list all note id in results page

--- SearchResultSection.ts
+++ SearchResultSection.ts
@@ -1,6 +1,8 @@
 ///<reference path="AppEvents.ts"/>
 ///<reference path="BodySection.ts"/>
 ///<reference path="CommandsSection.ts"/>
+///<reference path="AsyncUtil.ts"/>
+///<reference path="Storage.ts"/>
 
 namespace SearchResultSectionNamespace {
 
@@ -9,24 +11,35 @@ namespace SearchResultSectionNamespace {
     import setBody = BodySectionNamespace.setBody;
     import setCommandButtons = CommandsSectionNamespace.setCommandButtons;
     import register = AppEventsNamespace.register;
+    import r = Utility.r;
+    import getIDB = StorageNamespace.getIDB;
+    import iterateNotes = StorageNamespace.iterateNotes;
 
     let newNoteButton = document.createElement("button");
     newNoteButton.appendChild(document.createTextNode("New"));
+
+    function showAllResults(){
+        let resultLists = document.createElement("div");
+        r(function*(){
+            let idb = yield getIDB();
+            yield iterateNotes(idb, function(note: Note){
+                let item = document.createElement("div");
+                item.appendChild(document.createTextNode(note.id.toString()));
+                item.onclick = function() {
+                    broadcast(AppEvent.viewNote, note.id);
+                };
+                resultLists.appendChild(item);
+            });
+            setCommandButtons([newNoteButton]);
+            setBody(resultLists);
+        })
+    }
+
     newNoteButton.onclick = function(){
         broadcast(AppEvent.createNewNote);
     };
 
-    let bodyFrag = document.createDocumentFragment();
-    let getStarted = document.createElement("div");
-    getStarted.appendChild(document.createTextNode("Get Started"));
-    bodyFrag.appendChild(getStarted);
-
-    function setBlankResultsPage(){
-        setCommandButtons([newNoteButton]);
-        setBody(bodyFrag);
-    }
-
-    register(AppEvent.setBlankResultsPage, setBlankResultsPage);
+    register(AppEvent.resultsPage, showAllResults);
 
     //this seemly awkward useless function is called by App.ts to ensure that this search results section module is created first
     export function init(){}
