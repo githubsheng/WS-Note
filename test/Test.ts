e2afe55536714f1c84fb194bfba6328542ce86d0
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-07 00:07:18 +0800

    run digest test cases

--- test/Test.ts
+++ test/Test.ts
@@ -2,9 +2,11 @@
 ///<reference path="TestTextProcessor.ts" />
 ///<reference path="TestIndex.ts" />
 ///<reference path="TestContentTransformer.ts"/>
+///<reference path="TestDigest.ts"/>
 
 TestIndexNamespace.runIndexTest();
 TestTextProcessorNamespace.runTextProcessorTest();
 TestStorageNamespace.runStorageTest();
 TestContentTransformerNamespace.runContentTransformerTest();
+TestDigestNamespace.testDigest();
 
