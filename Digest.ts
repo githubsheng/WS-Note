5bdb448b3d36d4847180c54785ac309e6b3f0a3d
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-07 00:04:22 +0800

    implement digest for multiple key words

---
+++ Digest.ts
@@ -0,0 +1,127 @@
+///<reference path="CommonModels.ts"/>
+
+function findSmallestPartThatCoveringKeyWordSet(paragraph: string[], keyWords: Set<string>): string[]{
+    let keywordsToCount: Map<string, number> = new Map();
+    let left = 0, right = 0;
+    let start= -1, end = -1;
+    while(right < paragraph.length) {
+        while (right < paragraph.length && keywordsToCount.size < keyWords.size) {
+            if(keyWords.has(paragraph[right])){
+                keywordsToCount.set(
+                    paragraph[right],
+                    keywordsToCount.has(paragraph[right])
+                        ? keywordsToCount.get(paragraph[right]) + 1
+                        : 1);
+            }
+            ++right;
+        }
+
+        if(keywordsToCount.size === keyWords.size
+            && (start == -1 && end == -1)
+            || (right - 1 - left < end - start)) {
+            start = left;
+            end = right - 1;
+        }
+
+        while(left < right && keywordsToCount.size === keyWords.size) {
+            if(keyWords.has(paragraph[left])) {
+                let keywordCount = keywordsToCount.get(paragraph[left]);
+                keywordsToCount.set(paragraph[left], --keywordCount);
+                if(keywordCount == 0) {
+                    keywordsToCount.delete(paragraph[left]);
+                    if((start == -1 && end == -1)
+                        || right - 1 - left < end - start) {
+                        start = left;
+                        end = right - 1;
+                    }
+                }
+            }
+            ++left;
+        }
+    }
+    return paragraph.slice(start, end + 1);
+}
+
+function findSegmentsBetweenKeyWords(digest: string[], keyWords: Set<string>): string[][] {
+    let segments: string[][] = [];
+    let start = -1;
+    for(let i = 0; i < digest.length; i++) {
+        let word = digest[i];
+        if(keyWords.has(word)) {
+            if(start > 0) segments.push(digest.slice(start, i));
+            start = i + 1;
+        }
+    }
+    return segments;
+}
+
+function findKeyWordsInDisplayOrder(digest: string[], keyWords: Set<string>): string[] {
+    let result: string[] = [];
+    for(let word of digest) {
+        if(keyWords.has(word)) result.push(word);
+    }
+    return result;
+}
+
+function shrinkSegments(segments: string[][], requiredSegLengths: number[]) {
+    segments.forEach(function(s: string[], idx){
+        let requiredLength = requiredSegLengths[idx];
+        if(s.length > requiredLength) {
+            s.splice(Math.floor(requiredLength / 2), s.length - requiredLength, "...");
+        }
+    });
+}
+
+function appendKeyWordSpanToFrag(frag: DocumentFragment, keyWord: string) {
+    let keyWordSpan = document.createElement("span");
+    keyWordSpan.classList.add("keyWord");
+    keyWordSpan.appendChild(document.createTextNode(keyWord));
+    frag.appendChild(keyWordSpan);
+}
+
+function joinSegmentsWithKeyWords(segments: string[][], keyWords: string[]): DocumentFragment {
+    let frag = document.createDocumentFragment();
+    appendKeyWordSpanToFrag(frag, keyWords[0]);
+    for(let i = 0; i < segments.length; i++) {
+        frag.appendChild(document.createTextNode(segments[i].join("")));
+        appendKeyWordSpanToFrag(frag, keyWords[i+1]);
+    }
+    return frag;
+}
+
+function calculateSegmentLengths(tokenSegmentLengths: number[], requiredMaxTotalLength: number){
+    let maxSegLength = 0;
+    let totalSegLength = 0;
+    for(let i = 0; i < tokenSegmentLengths.length; i++) {
+        totalSegLength += tokenSegmentLengths[i];
+        maxSegLength = Math.max(maxSegLength, tokenSegmentLengths[i]);
+    }
+
+    if(totalSegLength < requiredMaxTotalLength) return tokenSegmentLengths;
+
+    let toRemove = totalSegLength - requiredMaxTotalLength;
+
+    while(toRemove > 0) {
+        if(maxSegLength <= 3) break; //each segment is so small it does not make sense to remove any tokens anymore.
+        for(let i = 0; i < tokenSegmentLengths.length; i++) {
+            if(tokenSegmentLengths[i] === maxSegLength) {
+                tokenSegmentLengths[i] = tokenSegmentLengths[i] - 1;
+                toRemove--;
+            }
+        }
+        maxSegLength--;
+    }
+    return tokenSegmentLengths;
+}
+
+function findDigestForMultipleKeyWords(components: Component[], keyWords: Set<string>) {
+    let componentsWithTokens = components.filter((c: Component) => {return c.tokens !== undefined});
+    let listOfTokens = componentsWithTokens.map((c: Component) => {return c.tokens.tokenValues});
+    let para: string[] = [].concat(...listOfTokens);
+    let digest = findSmallestPartThatCoveringKeyWordSet(para, keyWords);
+    let segmentBetweenKeyWords = findSegmentsBetweenKeyWords(digest, keyWords);
+    let keyWordsInDisplayOrder = findKeyWordsInDisplayOrder(digest, keyWords);
+    let requiredLengthForSegments = calculateSegmentLengths(segmentBetweenKeyWords.map((s: string[]) => s.length), 40);
+    shrinkSegments(segmentBetweenKeyWords, requiredLengthForSegments);
+    return joinSegmentsWithKeyWords(segmentBetweenKeyWords, keyWordsInDisplayOrder);
+}
---
+++ test/TestDigest.ts
@@ -0,0 +1,59 @@
+///<reference path="../CommonModels.ts"/>
+///<reference path="../Tokenizor.ts"/>
+///<reference path="../Digest.ts"/>
+///<reference path="TestUtil.ts"/>
+
+namespace TestDigestNamespace {
+
+    import tokenize = TokenizorNamespace.tokenize;
+    import shouldBeEqual = TestUtilNamespace.shouldBeEqual;
+    function createBRComponent(){
+        return {nodeName: "br"};
+    }
+
+    function createTextComponentWith(text: string):Component {
+        return {nodeName: "#text", value: text};
+    }
+
+    function createDummyTokenizedParagraph(): Component[]{
+        let c1 = createTextComponentWith("concat creates a new array consisting of the elements in the object on which it is called, followed in ");
+        c1.tokens = tokenize(c1);
+        let c2 = createBRComponent();
+        let c3 = createTextComponentWith("order by, for each argument, the elements of that argument (if the argument is an array) or the argument itself");
+        c3.tokens = tokenize(c3);
+        let c4 = createBRComponent();
+        let c5 = createTextComponentWith("(if the argument is not an array). concat does not alter this or any of the arrays provided as arguments but instead ");
+        c5.tokens = tokenize(c5);
+        let c6 = createBRComponent();
+        let c7 = createTextComponentWith("returns a shallow copy that contains copies of the same elements combined from the original arrays.");
+        c7.tokens = tokenize(c7);
+        let c8 = createBRComponent();
+        return [c1, c2, c3, c4, c5, c6, c7, c8];
+    }
+
+    export function testDigest(){
+        let components = createDummyTokenizedParagraph();
+        let keyWords = new Set<string>();
+        keyWords.add("array");
+        keyWords.add("elements");
+        keyWords.add("shallow");
+        let frag = findDigestForMultipleKeyWords(components, keyWords);
+        /*
+         * the generated nodes should be like this:
+         * <span class="keyWord">array</span>
+         * ). concat does not alter this... as arguments but instead returns a_    <- text node
+         * <span class="keyWord">shallow</span>
+         * _copy that contains copies of the same_     <- text node
+         * <span class="keyWord">elements</span>
+         * in the above: _ means whitespace
+         */
+        let generatedNodes = frag.childNodes;
+        shouldBeEqual(generatedNodes[0].firstChild.nodeValue, "array");
+        shouldBeEqual(generatedNodes[1].nodeValue, "). concat does not alter this... as arguments but instead returns a ");
+        shouldBeEqual(generatedNodes[2].firstChild.nodeValue, "shallow");
+        shouldBeEqual(generatedNodes[3].nodeValue, " copy that contains copies of the same ");
+        shouldBeEqual(generatedNodes[4].firstChild.nodeValue, "elements");
+    }
+
+
+}
