934a1956a5a73a0e80ae36120d44e208766498d6
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-08 18:38:55 +0800

    show ranked results in result page

--- SearchResultSection.ts
+++ SearchResultSection.ts
@@ -3,6 +3,8 @@
 ///<reference path="CommandsSection.ts"/>
 ///<reference path="AsyncUtil.ts"/>
 ///<reference path="Storage.ts"/>
+///<reference path="Rank.ts"/>
+///<reference path="Digest.ts"/>
 
 namespace SearchResultSectionNamespace {
 
@@ -14,32 +16,82 @@ namespace SearchResultSectionNamespace {
     import r = Utility.r;
     import getIDB = StorageNamespace.getIDB;
     import iterateNotes = StorageNamespace.iterateNotes;
+    import NoteScoreDetail = RankNamespace.NoteScoreDetail;
+    import search = RankNamespace.search;
+    import digest = DigestNamespace.digest;
+    import getNote = StorageNamespace.getNote;
 
     let newNoteButton = document.createElement("button");
     newNoteButton.appendChild(document.createTextNode("New"));
 
-    function showAllResults(){
-        let resultLists = document.createElement("div");
-        r(function*(){
-            let idb = yield getIDB();
-            yield iterateNotes(idb, function(note: Note){
-                let item = document.createElement("div");
-                item.appendChild(document.createTextNode(note.id.toString()));
-                item.onclick = function() {
-                    broadcast(AppEvent.viewNote, note.id);
-                };
-                resultLists.appendChild(item);
-            });
-            setCommandButtons([newNoteButton]);
-            setBody(resultLists);
-        })
+
+    function showGetStartedGuide(){
+        let getStartedGuideContainer = document.createElement("div");
+        getStartedGuideContainer.appendChild(document.createTextNode("Get Started"));
+        setBody(getStartedGuideContainer);
+    }
+
+    function* createNotePreview(noteScoreDetail: NoteScoreDetail, keyWords: Set<string>): IterableIterator<any> {
+        let preview = document.createElement("div");
+
+        let titleDiv = document.createElement("div");
+        titleDiv.appendChild(document.createTextNode(noteScoreDetail.noteName));
+        preview.appendChild(titleDiv);
+
+        let idb: IDBDatabase = yield getIDB();
+        let note: Note = yield getNote(idb, noteScoreDetail.noteId);
+
+        let digestContainer = document.createElement("div");
+        let digestFrag = digest(note.components, keyWords);
+        digestContainer.appendChild(digestFrag);
+        preview.appendChild(digestContainer);
+
+        return preview;
+    }
+
+    function showRankedResults(searchKeyWords: Set<string>){
+        const numberOfResultsShowedWhenMoreResultsPressed = 20;
+        let showedResults = 0;
+
+        let resultLists:HTMLDivElement = document.createElement("div");
+        let moreResultsButton = document.createElement("button");
+        let moreResultsContainer = document.createElement("div");
+        moreResultsContainer.appendChild(moreResultsButton);
+        resultLists.appendChild(moreResultsContainer);
+
+        function insertNotePreviews() {
+            let rankedResults: NoteScoreDetail[] = search(Array.from(searchKeyWords));
+            if(showedResults < rankedResults.length) {
+                let searchKeyWords;
+                r(function*(){
+                    let resultsToShow = rankedResults.slice(showedResults, showedResults += numberOfResultsShowedWhenMoreResultsPressed)
+                    for(let i = 0; i < resultsToShow.length; i++) {
+                        let notePreview = yield* createNotePreview(resultsToShow[i], searchKeyWords);
+                        resultLists.insertBefore(notePreview, moreResultsContainer);
+                    }
+                });
+                if(showedResults >= rankedResults.length) resultLists.removeChild(moreResultsContainer);
+            }
+        }
+
+        insertNotePreviews();
+        moreResultsButton.onclick = insertNotePreviews;
+
+        setBody(resultLists);
     }
 
     newNoteButton.onclick = function(){
         broadcast(AppEvent.createNewNote);
     };
 
-    register(AppEvent.resultsPage, showAllResults);
+    register(AppEvent.resultsPage, function(searchKeyWord?: Set<string>){
+        setCommandButtons([newNoteButton]);
+        if(searchKeyWord === undefined) {
+            showGetStartedGuide();
+        } else {
+            showRankedResults(searchKeyWord);
+        }
+    });
 
     //this seemly awkward useless function is called by App.ts to ensure that this search results section module is created first
     export function init(){}
