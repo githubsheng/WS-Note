24747060dc515ad054fb6be201786b2de392ba82
Wang Sheng <wang.sheng.unique@gmail.com>
2016-06-08 22:56:34 +0800

    bug fix: remove show more results button if there is no more results

--- SearchResultSection.ts
+++ SearchResultSection.ts
@@ -25,21 +25,21 @@ namespace SearchResultSectionNamespace {
     newNoteButton.appendChild(document.createTextNode("New"));
 
 
-    function showGetStartedGuide(){
+    function showGetStartedGuide() {
         let getStartedGuideContainer = document.createElement("div");
         getStartedGuideContainer.appendChild(document.createTextNode("Get Started"));
         setBody(getStartedGuideContainer);
     }
 
-    function* createNotePreview(noteScoreDetail: NoteScoreDetail, keyWords: Set<string>): IterableIterator<any> {
+    function* createNotePreview(noteScoreDetail:NoteScoreDetail, keyWords:Set<string>):IterableIterator<any> {
         let preview = document.createElement("div");
 
         let titleDiv = document.createElement("div");
         titleDiv.appendChild(document.createTextNode(noteScoreDetail.noteName));
         preview.appendChild(titleDiv);
 
-        let idb: IDBDatabase = yield getIDB();
-        let note: Note = yield getNote(idb, noteScoreDetail.noteId);
+        let idb:IDBDatabase = yield getIDB();
+        let note:Note = yield getNote(idb, noteScoreDetail.noteId);
 
         let digestContainer = document.createElement("div");
         let digestFrag = digest(note.components, keyWords);
@@ -49,7 +49,7 @@ namespace SearchResultSectionNamespace {
         return preview;
     }
 
-    function showRankedResults(searchKeyWords: Set<string>){
+    function showRankedResults(searchKeyWords:Set<string>) {
         const numberOfResultsShowedWhenMoreResultsPressed = 20;
         let showedResults = 0;
 
@@ -60,18 +60,17 @@ namespace SearchResultSectionNamespace {
         resultLists.appendChild(moreResultsContainer);
 
         function insertNotePreviews() {
-            let rankedResults: NoteScoreDetail[] = search(Array.from(searchKeyWords));
-            if(showedResults < rankedResults.length) {
-                let searchKeyWords;
-                r(function*(){
+            r(function*() {
+                let rankedResults:NoteScoreDetail[] = search(Array.from(searchKeyWords));
+                if (showedResults < rankedResults.length) {
                     let resultsToShow = rankedResults.slice(showedResults, showedResults += numberOfResultsShowedWhenMoreResultsPressed)
-                    for(let i = 0; i < resultsToShow.length; i++) {
+                    for (let i = 0; i < resultsToShow.length; i++) {
                         let notePreview = yield* createNotePreview(resultsToShow[i], searchKeyWords);
                         resultLists.insertBefore(notePreview, moreResultsContainer);
                     }
-                });
-                if(showedResults >= rankedResults.length) resultLists.removeChild(moreResultsContainer);
-            }
+                    if (showedResults >= rankedResults.length) resultLists.removeChild(moreResultsContainer);
+                }
+            });
         }
 
         insertNotePreviews();
@@ -80,19 +79,20 @@ namespace SearchResultSectionNamespace {
         setBody(resultLists);
     }
 
-    newNoteButton.onclick = function(){
+    newNoteButton.onclick = function () {
         broadcast(AppEvent.createNewNote);
     };
 
-    register(AppEvent.resultsPage, function(searchKeyWord?: Set<string>){
+    register(AppEvent.resultsPage, function (searchKeyWord?:Set<string>) {
         setCommandButtons([newNoteButton]);
-        if(searchKeyWord === undefined) {
+        if (searchKeyWord === undefined) {
             showGetStartedGuide();
         } else {
             showRankedResults(searchKeyWord);
         }
     });
 
-    //this seemly awkward useless function is called by App.ts to ensure that this search results section module is created first
-    export function init(){}
+//this seemly awkward useless function is called by App.ts to ensure that this search results section module is created first
+    export function init() {
+    }
 }
